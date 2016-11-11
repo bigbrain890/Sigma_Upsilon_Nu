28e212c36993da7a68f259e69658fb06c5c16914
bigbrain890 <bigbrain890@yahoo.com>
2016-11-10 20:59:15 -0500

    First Commit of Code
    Basic variable declaration as well as function naming for future
    developement. Very barebones. Should start to comment code in the
    future.

---
+++ Solar_Tracker/Solar_Tracker.ino
@@ -0,0 +1,59 @@
+#include <Servo.h>
+
+Servo radial, tilt;
+int photoSensor1 = A0;
+int photoSensor2 = A1;
+int photoSensor3 = A2;
+int photoSensor4 = A3;
+int photoSensor5 = A4;
+int photoSwitchTop = 3;
+int photoSwitchBottom = 4;
+int tempSensor = A5;
+float lightSensorData[4];
+float tiltUpSensor = 0;
+float tiltDownSensor = 0;
+float temp = 0;
+int lastHeading = 0;
+int targetHeading = 0;
+int currentLightDirection = 0;
+int lastLightDirection = 0;
+
+
+void setup() 
+{
+  radial.attach(9);
+  tilt.attach(10);
+  pinMode(photoSwitchTop, INPUT);
+  pinMode(photoSwitchBottom, INPUT);
+
+}
+
+void loop() 
+{
+  collectLightData();
+
+}
+
+void collectLightData()
+{
+  for(int i = 0; i <= 4; i++)
+  {   
+    lightSensorData[i] = analogRead("photoSensor"+'i');
+  }
+
+  temp = analogRead(tempSensor);
+  tiltUpSensor = digitalRead(photoSwitchTop);
+  tiltDownSensor = digitalRead(photoSwitchBottom);
+}
+
+void adjustTilt()
+{
+  
+}
+
+void activeSearch()
+{
+  
+}
+
+void 

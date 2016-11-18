d69a26253650608dcc76155a445deec645bd2392
bigbrain890 <bigbrain890@yahoo.com>
2016-11-18 16:53:56 -0500

    TEMP SENSOR WORKING WOO HOO
    I almost killed it at first haha. Had it plugged in backwards. Works
    well now. Change the request frequency to change how fast it reports the
    temperature.

--- Solar_Tracker/Solar_Tracker.ino
+++ Solar_Tracker/Solar_Tracker.ino
@@ -19,13 +19,15 @@ float lightSensorData[4];     // Array used for storing analog photosensor data
 float tiltUpSensor = 0;       // Variables for storing digital photosensor data
 float tiltDownSensor = 0;
 float temp = 0;               // Variable used for storing tempreature reading.
+float tempF = 0;
+float rawTemp = 0;
 int lastHeading = 0;          // Last known direction of solar panel
 int targetHeading = 0;        // Where we want the solar panel to point.
 int currentLightDirection = 0;
 int lastLightDirection = 0;   // The last direction the solar panel was towards
 unsigned long int currentTime = 0;
 unsigned long int lastReportTime;
-int reportFreq = 5000;
+int reportFreq = 500;
 
 
 
@@ -35,18 +37,21 @@ void setup()
   tilt.attach(10);                    
   pinMode(photoSwitchTop, INPUT);     // Make sure that digital pins for the two photoswitches know that they're inputs.
   pinMode(photoSwitchBottom, INPUT);
+  Serial.begin(9600);
 
 }
 
 void loop() 
 {
+  
   currentTime = millis();
   if((currentTime - lastReportTime) >= reportFreq)
   {
     dataReport();
     lastReportTime = currentTime; 
   }
-  collectLightData();
+  
+  //collectLightData();
 
 }
 
@@ -57,7 +62,6 @@ void collectLightData()
     lightSensorData[i] = analogRead("photoSensor"+'i');
   }
 
-  temp = analogRead(tempSensor);
   tiltUpSensor = digitalRead(photoSwitchTop);
   tiltDownSensor = digitalRead(photoSwitchBottom);
 }
@@ -74,6 +78,12 @@ void activeSearch()
 
 void dataReport()
 {
+  Serial.write(12);
+  rawTemp = analogRead(tempSensor);
+  float voltage = (rawTemp * 5.0) / 1024.0;
+  temp = (voltage - 0.5) * 100;
+  tempF = (temp * 9.0/5.0) + 32.0;
+  Serial.println(tempF);
   // Print out air temprature and direction sun based on pannel position.
 }
 

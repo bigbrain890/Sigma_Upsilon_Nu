a0689f9fb6d4e4187e29270440d8f171170affe0
bigbrain890 <bigbrain890@yahoo.com>
2016-12-02 17:05:36 -0500

    "Find Initial Bright Spot" Skeleton Code
    Put out some basic code to try and search for the brightest spot to
    start out. Have not tested it yet to see if it works.

--- Solar_Tracker/Solar_Tracker.ino
+++ Solar_Tracker/Solar_Tracker.ino
@@ -1,48 +1,51 @@
-#define SHUTDOWN_SERVO 5
-#define ACTIVATE_SERVO 4
-#define COMPARE_LIGHT_CHANGE 3
-#define CALC_TARG_ANGLE 2
-#define PULL_SENSOR_DATA 1
+#define SHUTDOWN_SERVO 1
+#define ACTIVATE_SERVO 2
+#define COMPARE_LIGHT_CHANGE 3 
+#define CALC_TARG_ANGLE 4
+#define PULL_SENSOR_DATA 5
+#define FIND_INITIAL_POSITION 6
+#define GO_TO_INITIAL_POSITION 7
+#define ADJUST_INITIAL_TILT 8
+#define ACTIVE_TRACKING 9
+
+#define VERTICLE 0
+#define FLAT 90
 #include <Servo.h>
 
 Servo radial, tilt;           // Make two servo objects.
-int photoSensor1 = A0;        // Pin allocation for first 5 photosensors. Going to use analog inputs to get variable readings.
-int photoSensor2 = A1;  
-int photoSensor3 = A2;
-int photoSensor4 = A3;
-int photoSensor5 = A4;
-int photoSwitchTop = 3;       // Put a couple of photo resistors on digital pins. Going to try and use them like photo switches.
-int photoSwitchBottom = 4;
+int photoSensorLeft = A0;        // Pin allocation for first 5 photosensors. Going to use analog inputs to get variable readings.
+int photoSensorRight = A1;  
+int photoSensorTop = A2;
+int photoSensorBottom = A3;
 int tempSensor = A5;          // Tempreature sensor for weather data we need to send.
 
 float lightSensorData[4];     // Array used for storing analog photosensor data
-float tiltUpSensor = 0;       // Variables for storing digital photosensor data
-float tiltDownSensor = 0;
 float temp = 0;               // Variable used for storing tempreature reading.
 float tempF = 0;
 float rawTemp = 0;
 int lastHeading = 0;          // Last known direction of solar panel
 int targetHeading = 0;        // Where we want the solar panel to point.
 int currentLightDirection = 0;
+int currentTilt = 0;
 int lastLightDirection = 0;   // The last direction the solar panel was towards
 unsigned long int currentTime = 0;
 unsigned long int lastReportTime;
-<<<<<<< HEAD
 int reportFreq = 5000;
+int startingLightDirection = 0;
+int maxTiltErrorThresh = 30;
+int startingLightTilt = 0;
 int noLightThresh = 0;        // Set value to minimum light in classroom. Do with function.
-=======
-int reportFreq = 500;
->>>>>>> origin/master
-
+int state = FIND_INITIAL_POSITION;
 
 
 void setup() 
 {
   radial.attach(9);                   // Attach the Servos to the perspective pins.
   tilt.attach(10);                    
-  pinMode(photoSwitchTop, INPUT);     // Make sure that digital pins for the two photoswitches know that they're inputs.
-  pinMode(photoSwitchBottom, INPUT);
   Serial.begin(9600);
+  tilt.write(45);
+  radial.write(0);
+  delay(500);
 
 }
 
@@ -50,25 +53,95 @@ void loop()
 {
   
   currentTime = millis();
-  if((currentTime - lastReportTime) >= reportFreq)
+  if ((currentTime - lastReportTime) >= reportFreq)
   {
     dataReport();
     lastReportTime = currentTime; 
   }
-  
-  //collectLightData();
 
+  if (state == FIND_INITIAL_POSITION)
+  {
+  	int brightestLight = 0;
+  	int currentLight = 0;
+
+    for(int i = 0; i <= 180; i++)
+    {
+    	currentLight = (analogRead(photoSensorLeft) + analogRead(photoSensorRight));
+    	if(currentLight > brightestLight)
+    	{
+    		startingLightDirection = i; 
+    		startingLightTilt = 45;
+    	}
+    	radial.write(i+1);
+    	delay(50);
+    }
+
+    tilt.write(135);
+    delay(400);
+
+    for(int i = 180; i <=0; i--)
+    {
+ 		currentLight = (analogRead(photoSensorLeft) + analogRead(photoSensorRight));
+ 		if(currentLight > brightestLight)
+ 		{
+ 			startingLightDirection = i;
+ 			startingLightTilt = 135;
+ 		}
+ 		radial.write(i-1);
+ 		delay(50);
+    }
+
+    state = GO_TO_INITIAL_POSITION;
+  }
+
+  else if (state == GO_TO_INITIAL_POSITION)
+  {
+  	radial.write(startingLightDirection);
+  	tilt.write(startingLightTilt);
+  	currentTilt = startingLightTilt;
+  	currentLightDirection = startingLightDirection;
+  	delay(400);
+  	state == ADJUST_INITIAL_TILT;
+
+  }
+
+  else if (state == ADJUST_INITIAL_TILT)
+  {
+  	int topValue = analogRead(photoSensorTop);
+  	int bottomValue = analogRead(photoSensorBottom);
+  	int difference = abs(bottomValue - topValue);
+
+  	if(difference > maxTiltErrorThresh)
+  	{
+  		if(topValue > bottomValue)
+  		{
+  			currentTilt--; 			// Change based on servo direction and stuff
+  		}
+  		else if (bottomValue > topValue )
+  		{
+  			currentTilt++;			// May have to flip this one with the other value
+  		}
+  		tilt.write(currentTilt);
+  	}
+
+  	else 
+  	{
+  		state = ACTIVE_TRACKING;
+  	}
+  	
+  }
+  else if (state == ACTIVE_TRACKING)
+  {
+
+  }
 }
 
 void collectLightData()
 {
-  for(int i = 0; i <= 4; i++)
-  {   
-    lightSensorData[i] = analogRead("photoSensor"+'i');
-  }
-
-  tiltUpSensor = digitalRead(photoSwitchTop);
-  tiltDownSensor = digitalRead(photoSwitchBottom);
+  lightSensorData[0] = analogRead(photoSensorLeft);
+  lightSensorData[1] = analogRead(photoSensorRight);
+  lightSensorData[2] = analogRead(photoSensorTop);
+  lightSensorData[3] = analogRead(photoSensorBottom);
 }
 
 void adjustTilt()
